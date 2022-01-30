6c590d312a72bd7e061a5fdc46a267559897e8cb
Natalia-Jorquera <natalia.fraj@gmail.com>
2022-01-30 22:06:55 +0100

    function to iterate over instances

--- dual.jl
+++ dual.jl
@@ -13,6 +13,7 @@ function duale(instance::String)
     end
     #model creation
     m=Model(CPLEX.Optimizer)
+    set_silent(m)
     # #variables
     @variable(m, x[1:n , 1:n], Bin)
     @variable(m, beta[1:n , 1:n], Bin)
@@ -21,20 +22,32 @@ function duale(instance::String)
     @variable(m, gamma>=0)
     @variable(m, epsilon[1:n]>=0)
     # #constraints
-    @constraint(m, [j in 1:n ; j != s && j != t], sum( x[i,j] for i in 1:n if d[i,j]!=0) == sum(x[j,i] for i in 1:n if d[i,j]!=0))
+    @constraint(m, [j in 1:n ; j != s && j != t], sum( x[i,j] for i in 1:n if d[i,j]!=0) == sum(x[j,i] for i in 1:n if d[j,i]!=0))
     @constraint(m, sum(x[s,j] for j in 1:n if d[s,j]!=0 )==1)
     @constraint(m, sum(x[j,t] for j in 1:n if d[j,t]!=0)==1)
     @constraint(m, [i in 1:n; i!=s] ,sum(x[i,j] for j in 1:n if d[i,j]!=0)==y[i])
     @constraint(m, [i in 1:n; i!=t] ,sum(x[j,i] for j in 1:n if d[j,i]!=0)==y[i])  
-    @constraint(m, [i in 1:n, j in 1:n], alpha +beta[i,j]>=d[i,j]*x[i,j])
+    @constraint(m, [i in 1:n, j in 1:n ; d[i,j]!=0], alpha +beta[i,j]>=d[i,j]*x[i,j])
     @constraint(m, sum((p[i]*y[i]+2*epsilon[i]) for i in 1:n)+ d2*gamma <=S)
     @constraint(m, [i in 1:n] , gamma + epsilon[i]>=ph[i]*y[i])  
-    @constraint(m, [i in 1:n, j in 1:n ; d[i,j]==0], x[i,j] ==0)
+    #@constraint(m, [i in 1:n, j in 1:n ; d[i,j]==0], x[i,j] ==0)
     #Objective
-    @objective(m, Min, sum(d[i,j]*x[i,j]+grandD[i,j]*beta[i,j] for i in 1:n, j in 1:n)+d1*alpha)
+    @objective(m, Min, sum(d[i,j]*x[i,j]+grandD[i,j]*beta[i,j] for i in 1:n, j in 1:n if d[i,j]!=0 )+d1*alpha)
     #Solve model
+    starting_time=time()
     optimize!(m)
+    final_time=time()-starting_time
     status = termination_status(m)
+    isOptimal = status == MOI.OPTIMAL
+    alpha_aux= JuMP.value(alpha)
+    beta_aux=Array{Int64,2}(zeros(n,n))
+    for i in 1:n
+        for j in 1:n
+            #println(JuMP.value(x[i,j]))
+            beta_aux[i,j]=JuMP.value(beta[i,j])
+        end
+    end
+    #println((sum(grandD[i,j]*beta_aux[i,j] for i in 1:n, j in 1:n if d[i,j]!=0 )+d1*alpha_aux))
     x_aux=Array{Int64,2}(zeros(n,n))
     for i in 1:n
         for j in 1:n
@@ -46,21 +59,21 @@ function duale(instance::String)
     for i in 1:n
         append!(y_aux, JuMP.value(y[i]))    
     end
-    cost=JuMP.objective_value(m)
-    return x_aux, y_aux, cost, status
+    z_aux=JuMP.objective_value(m)
+    # for i in 1:n
+    #     for j in 1:n
+    #         if x_aux[i,j]!=0
+    #             println("arc: ", (i,j))
+    #         end
+    #     end   
+    # end
+    # println("Cost: ",z_aux)
+    return z_aux, final_time, isOptimal
 
 end
-instance="20_USA-road-d.BAY.gr"
-x, y, cost , status = duale("20_USA-road-d.BAY.gr")
-#read("instances/20_USA-road-d.BAY.gr")
-for i in 1:size(x,1)
-    for j in 1:size(x,2)
-        print(x[i,j]," ")
-    end
-    println("")
-end
+#instance="20_USA-road-d.BAY.gr"
+#duale("20_USA-road-d.BAY.gr")
 
-println("Cost:" ,cost)  
 
 
 
